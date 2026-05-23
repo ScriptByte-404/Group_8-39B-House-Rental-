5565c5b981314c0acf7600164511c0a0ada8a23d
Avigya <avigyapoudel@gmail.com>
2026-05-23 23:00:33 +0545

    Fix merge conflict in logindata class

--- src/model/logindata.java
+++ src/model/logindata.java
@@ -1,70 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-<<<<<<< HEAD
 package model;
 
-/**
- *
- * @author Safal
- */
 public class logindata {
     private int user_id;
-    private String username;
-    private String email;
-    private String password;
-    
-    public int getUSerID() {
-        return user_id;
-    }
-    public void setUserID(int user_id) {
-        this.user_id=user_id;
-    }
-     public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username =username;
-        
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email =email;
-        
-    }public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public logindata(String username, String email, String password){
-        this.username=username;
-        this.email=email;
-        this.password=password;
-    }
-    
-    
-=======
-
-package model;
-
- public class logindata  {
-
-    private int id;
     private String username;
     private String email;
     private String phone;
     private String password;
     private String role;
 
-    public User() {
-    }
-
-    public User(String username, String email,String phone, String password, String role) {
-
+    public logindata(String username, String email, String phone, String password, String role) {
         this.username = username;
         this.email = email;
         this.phone = phone;
@@ -72,52 +16,22 @@ package model;
         this.role = role;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getUserId() { return user_id; }
+    public void setUserId(int user_id) { this.user_id = user_id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
->>>>>>> Safal_Branch
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
+
